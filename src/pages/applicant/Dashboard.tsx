adeac3253c67256a64587efa79710e4c4c05d989
jonmaxmore <104246416+jonmaxmore@users.noreply.github.com>
2025-09-25 22:03:33 +0700

    Create Applicant Dashboard

---
+++ src/pages/applicant/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ApplicantDashboard = () => {
+    return (
+        <div>
+            <h1>Applicant Dashboard</h1>
+            {/* TODO: Add application forms, status tracking, and certificate management */}
+        </div>
+    );
+};
+
+export default ApplicantDashboard;
