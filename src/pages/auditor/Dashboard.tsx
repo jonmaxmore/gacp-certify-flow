9dc9303ea3993299c7bfeb511f5b800db447face
jonmaxmore <104246416+jonmaxmore@users.noreply.github.com>
2025-09-25 22:10:51 +0700

    Create Auditor Dashboard component

---
+++ src/pages/auditor/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AuditorDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>แดชบอร์ดผู้ประเมิน</h1>
+            <p>งานประเมินและการตรวจสอบที่ได้รับมอบหมาย</p>
+        </div>
+    );
+};
+
+export default AuditorDashboard;
